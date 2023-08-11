b68b46a183d07ed86d7da508cebc13d172b8ee69
Hoon Wee <mrgravity817@gmail.com>
2023-08-12 00:18:43 +0900

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Hoon Wee on 2023/08/12.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
