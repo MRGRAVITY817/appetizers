808af9ed1e71ea746cd2d417b9906411a9be9cf1
Hoon Wee <mrgravity817@gmail.com>
2023-08-13 01:55:06 +0900

    feat: add empty states view

---
+++ Appetizers/Views/Empty States/EmptyStates.swift
@@ -0,0 +1,42 @@
+//
+//  EmptyStates.swift
+//  Appetizers
+//
+//  Created by Hoon Wee on 2023/08/13.
+//
+
+import SwiftUI
+
+struct EmptyStates: View {
+    
+    let imageName: String
+    let message: String
+    
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 150)
+                
+                Text(message)
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .padding()
+            }
+            
+        }
+    }
+}
+
+struct EmptyStates_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyStates(imageName: "empty-order", message: "This is our test message, blah blah any sentenceeeee.")
+    }
+}
---
+++ Appetizers/Views/Empty States/EmptyView.swift
@@ -0,0 +1,20 @@
+//
+//  EmptyView.swift
+//  Appetizers
+//
+//  Created by Hoon Wee on 2023/08/13.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmptyView()
+    }
+}
