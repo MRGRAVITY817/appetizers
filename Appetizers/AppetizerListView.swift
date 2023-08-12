57e5e6a450fd2c2eca96eb00566a85ee0fd01b56
Hoon Wee <mrgravity817@gmail.com>
2023-08-12 13:57:17 +0900

    feat: fetch data from server

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,12 +8,35 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State private var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationView {
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
             .navigationTitle("🍿 Appetizers")
+        }
+        .onAppear {
+            getAppetizers()
+        }
+    }
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            // Fetching happens in backend thread
+            // but assigning state happens in main thread.
+            //
+            // So we use DispatchQueue to put in main thread.
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
         }
     }
 }
