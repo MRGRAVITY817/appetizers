a18c2eb7702fc62a75b804f667b86f43eac5dcd0
Hoon Wee <mrgravity817@gmail.com>
2023-08-12 22:22:11 +0900

    feat: blur the list and disable scrolling

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -22,10 +22,12 @@ struct AppetizerListView: View {
                         }
                 }
                 .navigationTitle("🍿 Appetizers")
+                .disabled(isShowingDetail)
             }
             .onAppear {
                 viewModel.getAppetizers()
             }
+            .blur(radius: isShowingDetail ? 20 : 0)
             
             if isShowingDetail {
                 AppetizerDetailView(appetizer: MockData.sampleAppetizer,
