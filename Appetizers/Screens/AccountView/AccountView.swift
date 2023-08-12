db0916f682898d11e270217776ecd6ecc23e1c33
Hoon Wee <mrgravity817@gmail.com>
2023-08-13 00:17:45 +0900

    feat: add account form

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("😛 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                        .disableAutocorrection(true)
+                    TextField("Last Name", text: $lastName)
+                        .disableAutocorrection(true)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthDate, displayedComponents: .date)
+                    
+                    Button {
+                        print("Save")
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequest Refills", isOn: $frequentRefills)
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+            }
+            .navigationTitle("😛 Account")
         }
     }
 }
