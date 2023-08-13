11a37855a42a9508b56ad8925c7523b9335887ae
Hoon Wee <mrgravity817@gmail.com>
2023-08-13 16:59:27 +0900

    feat: create standard button style extension

--- Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -44,10 +44,11 @@ struct AppetizerDetailView: View {
                 order.add(appetizer)
                 isShowingDetail = false
             } label: {
-                APButton(title: "$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
             }
+            // .modifier(StandardButtonStyle())
+            .standardButtonStyle()
             .padding(.bottom, 30)
-            
         }
         .frame(width: 300, height: 525)
         .background(Color(.systemBackground))
